91a1e6bb264aaf05e49b75baf07c37a1cc314cad
cdr <1254688559@qq.com>
2019-12-08 19:31:39 +0800

    removeDuplicates

---
+++ removeDuplicates2/removeDuplicates.c
@@ -0,0 +1,27 @@
+int removeDuplicates(int* nums, int numsSize){
+
+if(numsSize==0)
+{return 0;}
+else if(numsSize==1)
+    {return 1;}
+
+
+
+int j=1;
+int i=0;
+int n=0;
+for(j=0;j<numsSize;j++)
+{
+if(nums[i]!=nums[j] )//¼ì²é
+{
+    i++;
+    nums[i]=nums[j];
+    n++;
+}
+
+}
+return i+1;
+}
+
+
+
